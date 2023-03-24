39a1510bfe0439644d08bef2d6c3030a7ae3787b
Gustavo Duarte <gutorap.gh@gmail.com>
2023-03-24 17:25:55 -0300

    fix Zod validation for optional author field

--- src/validators/postsValidator.ts
+++ src/validators/postsValidator.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 const validator = z.object({
   title: z.string().min(1).max(60),
   content: z.string().min(1).max(280),
-  author: z.string().min(1).max(20),
+  author: z.string().min(1).max(20).optional(),
 })
 
 export default validator
