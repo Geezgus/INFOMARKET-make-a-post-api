408854d3d584c9adec5d934e3a0408798d2bdeb7
Gustavo Duarte <gutorap.gh@gmail.com>
2023-03-23 22:53:51 -0300

    start server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('this')
+import express from 'express'
+import { Server } from 'http'
+import { AddressInfo } from 'net'
+
+const app = express()
+app.use(express.json())
+const port = process.env.PORT || 3333
+
+app.listen(port, function (this: Server) {
+  console.log('listening on port', (this.address() as AddressInfo).port)
+})
